1c423b3b4b3dbcf83f95f5d235091d9673a0bf94
Darius Williams <137736096+dwdsigned@users.noreply.github.com>
2023-12-09 15:50:36 -0500

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,18 @@
+const localQuotes = [
+    {
+        text: "The way to get started is to quit talking and begin doing.",
+        author: "Walt Disney"
+    },
+    {
+        text: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+        author: "James Cameron"
+    },
+    {
+        text: "Well done is better than well said.",
+        author: "Benjamin Franklin"
+    },
+    {
+        text: "Be yourself; everyone else is already taken.",
+        author: "Oscar Wilde"
+    }
+]
